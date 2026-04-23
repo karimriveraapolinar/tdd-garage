e883ba7d64068a9277d4a1cc1c88c87a18a7f13e
kariveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-23 08:08:13 -0400

    failing calculate_fee value error test

--- test_garage.py
+++ test_garage.py
@@ -1,4 +1,11 @@
 from garage import calculate_fee
+import pytest
+
 
 def test_calculate_fee():
     assert calculate_fee(3,5) == 15
+
+
+def test_calculate_fee_value_error():
+    with pytest.raises(ValueError):
+        calculate_fee(-3,5)
