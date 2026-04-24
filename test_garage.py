975d70ffc5b18007eb3722b225505b9137150e73
kariveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-24 07:44:41 -0400

    test_enter_garage_type_error failing

--- test_garage.py
+++ test_garage.py
@@ -25,3 +25,8 @@ def test_enter_garage_value_error():
     with pytest.raises(ValueError):
         garage1 = {"capacity": 0, "cars": {}}
         enter_garage(garage1, 'Bugatti17', 13)
+
+def test_enter_garage_type_error():
+    with pytest.raises(TypeError):
+        garage1 = {"capacity": 10, "cars": {}}
+        enter_garage(garage1, 'Bugatti17', "thirteen")
